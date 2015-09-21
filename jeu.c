55b60d5fd58a52aca2646916a5809ccb55edb64a
Antoine Grandjacquot <antoine.grandjacquot9@etu.univ-lorraine.fr>
2015-09-21 19:02:59 +0200

    Fail du dernier commit, je re essaye

--- jeu.c
+++ jeu.c
@@ -2,9 +2,124 @@
 #include <stdio.h>
 #include "jeu.h"
 
-/*******************************************************************************************/
+#define TAB_MAX 1000
+
+SDL_Surface* Load_image(const char* image) {
+  SDL_Surface* res;
+  SDL_Surface* temp = SDL_LoadBMP(image);
+  res = SDL_DisplayFormat(temp);
+  SDL_FreeSurface(temp);
+  return res;
+}
+
+//Fonctions de LoadMap
+void LoadMap_tiles(FILE* Fichier,Map* map) //Sous fonction qui charge les tiles dans un tableau
+{
+  int nbtile, i, j;
+  char tab[TAB_MAX];  
+  char tab1[TAB_MAX]; // Tableaux pour stocker lignes Fichier 
+  fscanf(Fichier, "%s", tab); // #images_tiles
+  fscanf(Fichier, "%s", tab); // nom image bmp
+  map->tileset = Load_image(tab); //On charge l'image indiquée dans le txt
+  fscanf(Fichier, "%d %d", &map->nbtilesX, &map->nbtilesY);
+  map->tile_width = map->tileset->w / map->nbtilesX; //on obtient la largeur d'un tile
+  map->tile_height = map->tileset->h / map->nbtilesY; //on obtient la hauteur d'un tile
+  map->tab_props = malloc(map->nbtilesX * map->nbtilesY * sizeof(TileProp));
+  for(j = 0, nbtile = 0; j < map->nbtilesY; j++)
+    {
+      for(i = 0; i < map->nbtilesX; i++, nbtile++)
+	{
+	  map->tab_props[nbtile].R.w = map->tile_width;
+	  map->tab_props[nbtile].R.h = map->tile_height;
+	  map->tab_props[nbtile].R.x = i * map->tile_width;
+	  map->tab_props[nbtile].R.y = j * map->tile_height;
+	  fscanf(Fichier,"%s %s",tab,tab1);
+	  map->tab_props[nbtile].type = 0;
+	  if (strcmp(tab1,"terrain")==0)
+	    map->tab_props[nbtile].type = 1;
+	}
+    }
+}
+
+
+
+void LoadMap_structure(FILE* Fichier,Map* map) //Charge les tiles dans le tableau final
+{
+  int i,j;
+  char tab[TAB_MAX];  
+  fscanf(Fichier, "%s", tab); // On change de partie du Fichier (#monde)
+  fscanf(Fichier, "%d %d", &map->nbtiles_largeur_monde, &map->nbtiles_hauteur_monde);
+  map->monde = malloc(map->nbtiles_largeur_monde * sizeof(Uint16*));
+  for(i = 0; i < map->nbtiles_largeur_monde; i++)
+    map->monde[i] = malloc(map->nbtiles_hauteur_monde * sizeof(Uint16));
+  for(j = 0; j < map->nbtiles_hauteur_monde; j++)
+    {
+      for(i = 0; i < map->nbtiles_largeur_monde; i++)
+	{
+	  int tmp;
+	  fscanf(Fichier, "%d", &tmp);
+	  if (tmp >= map->nbtilesX * map->nbtilesY)
+	    {
+	      printf("Il n'y a pas autant de tiles !\n");
+	      SDL_Quit();
+	    }
+	  map->monde[i][j] = tmp;
+	}
+    }
+}
+
+
+
+Map* LoadMap(const char* monde) //affecte le Fichier txt à la map
+{
+  FILE* Fichier;
+  Map* map;
+  Fichier = fopen(monde,"r");
+  if (!Fichier)
+    {
+      printf("Fichier %s introuvable !! \n",monde);
+      SDL_Quit();
+    }
+  map = malloc(sizeof(Map));
+  LoadMap_tiles(Fichier,map);
+  LoadMap_structure(Fichier,map);
+  fclose(Fichier);
+  return map;
+}
+
+
+int PrintMap(Map* map,SDL_Surface* screen) //affiche la map
+{
+  int i,j;
+  SDL_Rect Rect_dest;
+  int numero_tile;
+  for(i = 0; i < map->nbtiles_largeur_monde; i++)
+    {
+      for(j = 0; j < map->nbtiles_hauteur_monde; j++)
+	{
+	  Rect_dest.x = i * map->tile_width;
+	  Rect_dest.y = j * map->tile_height;
+	  numero_tile = map->monde[i][j];
+	  SDL_BlitSurface(map->tileset, &(map->tab_props[numero_tile].R), screen, &Rect_dest);
+	}
+    }
+  return 0;
+}
+
+int FreeMap(Map* map)
+{
+  int i;
+  SDL_FreeSurface(map->tileset);
+  for(i = 0; i < map->nbtiles_hauteur_monde; i++)
+    free(map->monde[i]);
+  free(map->monde);
+  free(map->tab_props);
+  free(map);
+  return 0;
+}
+
+
 /******************************          Programme          *******************************/
-/*******************************************************************************************/
 
 /* varaible pour les phases de jeux */
 
@@ -26,10 +141,10 @@ void update_events(char* keys)
 
     case SDL_KEYDOWN:
       switch (event.key.keysym.sym) {
-
+	
       case SDLK_q:
-				gameover = 1;
-				break;
+	gameover = 1;
+	break;
       }
       keys[event.key.keysym.sym] = 1;
       break;
@@ -46,10 +161,10 @@ void update_events(char* keys)
 
 int main(int argc, char* argv[])
 {
-  SDL_Surface *screen, *temp, *background;
+  SDL_Surface *screen, *temp;
   int colorkey;
 
-  
+  Map* map;
   /* set the title bar */
   SDL_WM_SetCaption("Tower Defense", "SDL Animation");
   
@@ -62,14 +177,7 @@ int main(int argc, char* argv[])
 
   /* **********************   LOAD IMAGE ******************* */
 
-
-  /* load background */
-  temp  = SDL_LoadBMP("background.bmp");
-  background = SDL_DisplayFormat(temp);
-  SDL_FreeSurface(temp);
-  
-
-
+ 
 
   /* ********************   colorkey ******************* */
 
@@ -79,38 +187,39 @@ int main(int argc, char* argv[])
   colorkey =  SDL_MapRGB(screen->format, 255, 0, 255);
 
   char key[SDLK_LAST] = {0};
-	gameover = 0;
-
-
-	/* ******************** boucle principale ******************* */
-
- 
-	/* message pump */
-	while (!gameover)
-	{
-
-
-	/* initialize SDL */
-	SDL_Init(SDL_INIT_VIDEO);
-
-
-	/* look for an event */
-	update_events(key);
-	
-	
-	/* draw the background */
-	SDL_BlitSurface(background, NULL, screen, NULL);
-	
-	
-	/* update the screen */
-	SDL_UpdateRect(screen, 0, 0, 0, 0);
-	SDL_Delay(5);
+  gameover = 0;
+  
+  
+  /* ******************** boucle principale ******************* */
+  
+  
+  /* message pump */
+  while (!gameover)
+    {
       
-    
-  }
-  
-  
-  
+      
+      /* initialize SDL */
+      SDL_Init(SDL_INIT_VIDEO);
+      
+      
+      /* look for an event */
+      update_events(key);
+      
+      
+      /* draw the background 
+	 SDL_BlitSurface(background, NULL, screen, NULL);
+      */
+      map = LoadMap("monde.txt");
+      PrintMap(map,screen);
+      SDL_Flip(screen);
+      
+      
+      /* update the screen */
+      SDL_UpdateRect(screen, 0, 0, 0, 0);
+      SDL_Delay(5);
+      
+      
+    }
   
   
   
@@ -118,9 +227,8 @@ int main(int argc, char* argv[])
   /* ****************************************************************************************************************************** */
   /* ***********************************     Clean Up    ************************************************************************** */
   /* ****************************************************************************************************************************** */
-
- 
-  SDL_FreeSurface(background);
+  
+  
   SDL_Quit();
   
   return 0;
--- jeu.h
+++ jeu.h
@@ -1,16 +1,33 @@
-
-
 #include "SDL.h"
 #include <math.h> 
 #include <time.h>
 
-#define SCREEN_WIDTH  640
+#define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
+/************************** Structs**********************************/
 
+typedef struct
+{
+  SDL_Rect R;
+  int type;
+} TileProp;
+
+typedef struct
+{
+  int tile_width, tile_height;
+  int nbtilesX,nbtilesY;
+  SDL_Surface* tileset;
+  TileProp* tab_props;
+  Uint16** monde;
+  int nbtiles_largeur_monde,nbtiles_hauteur_monde;
+} Map;
 
 /**************************Headers**********************************/
 
+Map* LoadMap(const char* Fichier);
+int PrintMap(Map* map,SDL_Surface* screen);
+int FreeMap(Map* map);
 
 /******************************* SDL *********************************/
 

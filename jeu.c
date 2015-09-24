f517a1d72270fb524df8d6379aaf6a3ab63da4cd
Antoine Grandjacquot <grandjac3u@fst-ats-311-l19.fst.site.univ-lorraine.fr>
2015-09-24 18:23:47 +0200

    Ajout d'un monstre qui suit le chemin

--- jeu.c
+++ jeu.c
@@ -48,7 +48,7 @@ void LoadMap_structure(FILE* Fichier,Map* map) //Charge les tiles dans le tablea
   int i,j;
   char tab[TAB_MAX];  
   fscanf(Fichier, "%s", tab); // On change de partie du Fichier (#monde)
-  fscanf(Fichier, "%d %d", &map->nbtiles_largeur_monde, &map->nbtiles_hauteur_monde);
+  fscanf(Fichier, "%d %d", &map->nbtiles_largeur_monde, &map->nbtiles_hauteur_monde); //largeur et hauteur du tableau monde
   map->monde = malloc(map->nbtiles_largeur_monde * sizeof(Uint16*));
   for(i = 0; i < map->nbtiles_largeur_monde; i++)
     map->monde[i] = malloc(map->nbtiles_hauteur_monde * sizeof(Uint16));
@@ -119,6 +119,106 @@ int FreeMap(Map* map)
 }
 
 
+s_Mob mob_spawn(s_Mob s_mob, int taillew, int tailleh) { //gère l'apparition du vaisseau
+  /* set vaisseau speed */
+  s_mob.vit.x = 0.2;
+  s_mob.vit.y = 0.0;
+
+  /* set sprite position */
+  s_mob.coords.x = -30;
+  s_mob.coords.y = 120;
+  
+  /* set sprite animation frame */
+  s_mob.rcSrc.x = 0;
+  s_mob.rcSrc.y = 0;
+  s_mob.rcSrc.w = taillew;
+  s_mob.rcSrc.h = tailleh;
+
+  s_mob.animation = 0;
+  
+  return s_mob;
+}
+s_Mob mob_deplacement(s_Mob s_mob) { 
+
+
+  s_mob.coords.x = s_mob.coords.x + s_mob.vit.x;
+  s_mob.coords.y = s_mob.coords.y + s_mob.vit.y;
+  
+  
+  return s_mob;
+}
+
+s_Mob mob_parcours(s_Mob s_mob, Map *map){
+  
+  int x, y, marge, haut, bas, gauche, droite;
+  
+ 
+  x = (int) (s_mob.coords.x + CREEP_WIDTH/2 ) / TILE_SIZE;
+  y = (int) (s_mob.coords.y +  CREEP_HEIGHT/2) / TILE_SIZE;
+      
+  
+  if (s_mob.coords.x > TILE_SIZE){
+    haut = map->monde[x][y-1];
+    gauche = map->monde[x-1][y];
+    bas = map->monde[x][y+1];
+    droite = map->monde[x+1][y];
+
+    float creep_speed = 0.4;
+    marge = 8;
+
+   if (map->tab_props[gauche].type == 0 && s_mob.vit.y > 0 && s_mob.coords.y >= y*TILE_SIZE - marge) {
+      s_mob.vit.x = -creep_speed;
+      s_mob.vit.y = 0;
+    }
+    if (map->tab_props[gauche].type == 0 && s_mob.vit.y < 0 && s_mob.coords.y <= y*TILE_SIZE - marge) {
+      s_mob.vit.x = -creep_speed;
+      s_mob.vit.y = 0;
+    }
+    if (map->tab_props[bas].type == 0 && s_mob.vit.x > 0 && s_mob.coords.x >= x*TILE_SIZE + marge){
+      s_mob.vit.x = 0;
+      s_mob.vit.y = creep_speed;
+    }
+    if (map->tab_props[bas].type == 0 && s_mob.vit.x < 0 && s_mob.coords.x <= x*TILE_SIZE + marge){
+      s_mob.vit.x = 0;
+      s_mob.vit.y = creep_speed;
+    }
+    if (map->tab_props[haut].type == 0 && s_mob.vit.x > 0 && s_mob.coords.x >= x*TILE_SIZE + marge){
+      s_mob.vit.x = 0;
+      s_mob.vit.y = -creep_speed;
+    }
+    if (map->tab_props[haut].type == 0 && s_mob.vit.x < 0 && s_mob.coords.x <= x*TILE_SIZE + marge){
+      s_mob.vit.x = 0;
+      s_mob.vit.y = -creep_speed;
+    }
+
+    if (map->tab_props[droite].type == 0 && s_mob.vit.y > 0 && s_mob.coords.y >= (y*TILE_SIZE - marge)) {
+      s_mob.vit.x = creep_speed;
+      s_mob.vit.y = 0;
+    }
+    if (map->tab_props[droite].type == 0 && s_mob.vit.y < 0 && s_mob.coords.y <= (y*TILE_SIZE - marge)) {
+      s_mob.vit.x = creep_speed;
+      s_mob.vit.y = 0;
+    }
+ 
+
+
+  }
+
+  return s_mob;
+}
+
+s_Mob mob_animation(s_Mob s_mob) {
+
+  s_mob.animation += 1;
+  if (s_mob.animation >= 45) 
+    s_mob.animation = 0;
+
+  s_mob.rcSrc.x = (s_mob.animation/15)* s_mob.rcSrc.w;
+
+  return s_mob;
+}
+
+
 /******************************          Programme          *******************************/
 
 /* varaible pour les phases de jeux */
@@ -162,8 +262,9 @@ void update_events(char* keys)
 
 int main(int argc, char* argv[])
 {
-  SDL_Surface *screen, *temp, *creeper;
+  SDL_Surface *screen, *temp;
   int colorkey;
+  s_Mob creep;
 
   Map* map, *map_objet;
   /* set the title bar */
@@ -175,12 +276,12 @@ int main(int argc, char* argv[])
   /* set keyboard repeat */
   SDL_EnableKeyRepeat(70, 70);
   
-
+  int xmin, ymin;
   /* **********************   LOAD IMAGE ******************* */
   map = LoadMap("monde.txt");
   map_objet = LoadMap("objet.txt");
   
-  creeper = Load_image("sprite_creeper.bmp");
+  creep.mob = Load_image("sprite_creeper.bmp");
   
 
   /* ********************   colorkey ******************* */
@@ -190,7 +291,7 @@ int main(int argc, char* argv[])
   /* Pour le ROSE */
   colorkey =  SDL_MapRGB(screen->format, 255, 0, 255);
   SDL_SetColorKey(map_objet->tileset, SDL_SRCCOLORKEY | SDL_RLEACCEL,colorkey);
-  SDL_SetColorKey(creeper, SDL_SRCCOLORKEY | SDL_RLEACCEL,colorkey);
+  SDL_SetColorKey(creep.mob, SDL_SRCCOLORKEY | SDL_RLEACCEL,colorkey);
   char key[SDLK_LAST] = {0};
   gameover = 0;
   
@@ -198,6 +299,8 @@ int main(int argc, char* argv[])
   /* ******************** boucle principale ******************* */
   
   
+  creep = mob_spawn(creep, CREEP_WIDTH, CREEP_HEIGHT);
+
   /* message pump */
   while (!gameover)
     {
@@ -218,11 +321,23 @@ int main(int argc, char* argv[])
       PrintMap(map_objet,screen);
 
      /* draw the creeper */
-      SDL_BlitSurface(creeper, NULL, screen, NULL);
+      creep = mob_deplacement(creep);
+      creep = mob_animation(creep);
+
+      creep.rcSprite.x = (int) creep.coords.x;
+      creep.rcSprite.y = (int) creep.coords.y;
+      SDL_BlitSurface(creep.mob, &creep.rcSrc, screen, &creep.rcSprite);
 
       SDL_Flip(screen);
       
-      
+      creep = mob_parcours(creep,map);
+      /* test */
+      xmin = (int) creep.coords.x / 32;
+      ymin = (int) creep.coords.y / 32;
+
+      //printf(" %d %d \n", xmin, ymin);
+
+
       /* update the screen */
       SDL_UpdateRect(screen, 0, 0, 0, 0);
       SDL_Delay(5);
@@ -237,7 +352,7 @@ int main(int argc, char* argv[])
   /* ***********************************     Clean Up    ************************************************************************** */
   /* ****************************************************************************************************************************** */
   
-  SDL_FreeSurface(creeper);
+  SDL_FreeSurface(creep.mob);
   SDL_Quit();
   
   return 0;
--- jeu.h
+++ jeu.h
@@ -2,8 +2,13 @@
 #include <math.h> 
 #include <time.h>
 
+#define TILE_SIZE 32
+
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+
+#define CREEP_WIDTH 16
+#define CREEP_HEIGHT 32
 
 /************************** Structs**********************************/
 
@@ -23,11 +28,31 @@ typedef struct
   int nbtiles_largeur_monde,nbtiles_hauteur_monde;
 } Map;
 
+
+typedef struct
+{
+  float x;
+  float y;
+} s_Floatpos;
+
+typedef struct
+{
+  s_Floatpos coords;
+  s_Floatpos vit;
+  int animation;
+  int direction;
+  SDL_Rect rcSrc;
+  SDL_Rect rcSprite;
+  SDL_Surface *mob;
+} s_Mob;
+
+
 /**************************Headers**********************************/
 
 Map* LoadMap(const char* Fichier);
 int PrintMap(Map* map,SDL_Surface* screen);
 int FreeMap(Map* map);
+s_Mob mob_spawn(s_Mob s_mob, int taillew, int tailleh);
 
 /******************************* SDL *********************************/
 
